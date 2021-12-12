5aa04988045d2bba9e3871c335843568d869c621
FrozenFrost6 <44104814+FrozenFrost6@users.noreply.github.com>
2021-12-12 22:49:50 +0530

    Add files via upload

--- Engine.java
+++ Engine.java
@@ -1,6 +1,18 @@
+import Inventories.Inventory;
+import Inventories.Products.Product;
+
 public class Engine {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Store store = new Store();
+        Inventory inventory = new Inventory("products.csv");
+        Product product = inventory.getProduct(205001006);
+        System.out.println(inventory.removeStock(205001006, 1));
+        
+        if(product != null) { // upgrade to exception
+            System.out.println(product);
+        } else {
+            System.out.println("product not found");
+        }
+
+        inventory.saveArray("products.csv");
     }
 }
