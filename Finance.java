37c8322736c6edd2300397c698cffa22c226c064
kavinsg <94192603+kavinsg@users.noreply.github.com>
2021-12-13 00:30:02 +0530

    Add files via upload

---
+++ Finance.java
@@ -0,0 +1,53 @@
+package JAVA.MiniProject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.util.*;
+import JAVA.MiniProject.Inventory.Product.Product;
+
+public class Finance{
+    double bankBalance;
+    File Balance;
+    Scanner parser;
+    double tempBalance;
+    //tempBalance = bankBalance;
+    public Finance(String balanceFilename){
+        try{
+            parser = new Scanner(new File(balanceFilename));
+            bankBalance = parser.nextDouble();
+            System.out.println(bankBalance);
+        }catch(FileNotFoundException e){
+            System.out.println("File not Found");
+        }
+    }
+
+    void payEmployees(Employee[] employees){
+        for (int i=0;i<employees.length;i++){
+            bankBalance-=employees[i].getWage();
+        }
+    }
+
+    //Add balance from item purchases
+    void purchaseItems(ArrayList<Product> shoppingList){
+        for (int i=0;i<shoppingList.size();i++){
+            bankBalance+=shoppingList.get(i).getPrice()*shoppingList.get(i).getNumber();
+        }
+    }
+
+    //Compare before and after for Profit or Loss
+    void profitLoss(){
+        double difference = bankBalance - tempBalance;
+        if (difference<0)       System.out.printf("Loss of %d"+difference);
+        else if (difference>0)  System.out.printf("Profit of %d"+difference);
+        else                    System.out.printf("No Profit");
+    }
+
+    //Restock items and remove amount from balance
+    void purchaseStock(ArrayList<Product> newStock){
+        for (int i=0;i<newStock.size();i++){
+            bankBalance-=newStock.get(i).getPrice()*newStock.get(i).getNumber();
+        }
+    }
+}
