9108e40d6380d85b8df139a8eabfb275ca4953c8
FrozenFrost6 <44104814+FrozenFrost6@users.noreply.github.com>
2021-12-12 23:41:09 +0530

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -1,11 +1,11 @@
-import Inventories.Inventory;
-import Inventories.Products.Product;
+import Inventory.Inventory;
+import Inventory.Product.Product;
 
 public class Engine {
     public static void main(String[] args) {
-        Inventory inventory = new Inventory("products.csv");
-        Product product = inventory.getProduct(205001006);
-        System.out.println(inventory.removeStock(205001006, 1));
+        Inventory inventory = new Inventory("products.csv", "restock.csv");
+        Product product = inventory.getProduct(205001003);
+        System.out.println(inventory.removeStock(205001003, 1));
         
         if(product != null) { // upgrade to exception
             System.out.println(product);
